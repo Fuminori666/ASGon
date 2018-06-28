a80babb0796060824f511dd2e27fd34d99d46237
fuminori666 <fuminori.kun@gmail.com>
2018-06-28 23:58:45 +0200

    Issues #1 Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="ASGon",
+    version="0.1",
+    author="Anton Chebanov",
+    author_email="anton.chebanov01@gmail.com",
+    description=("Project for ASGon airsoft club "),
+    packages=['Flask', 'FlaskForm']
+)
